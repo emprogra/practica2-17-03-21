b97731ffc30da9ddfff6dfc57366aed8d5bbfa75
Emily <a2017110728@estudiantes.upsa.edu.bo>
2021-03-17 21:41:05 -0400

    Creando el DataContext

---
+++ ApiDato/practica2/Models/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace practica2.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<practica2.Models.ross> ross { get; set; }
+    }
+}
